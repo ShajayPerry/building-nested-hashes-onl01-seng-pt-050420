6783a51e53e3c3739443ac9977a2923613a31dad
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-18 00:54:59 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++
@@ -1,7 +0,0 @@
-def bonus
-{name: "Benvolio", age: "17", attitude: "worried"},
-{name: "Steven", age: "30", attitude: "confused"}
-:capulet => {
-  :patriarch => {name: "Lord Capulet", age: "50"},
-  :matriarch => {name: "Lady Capulet", age: "51"},
-  
